9b82900530f0a75fc78500dd699d2c3a9de9eeb3
uKotka <kotek@ukotka.com>
2026-02-08 11:02:44 +0100

    chore: update dependencies and remove temporary files

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_localization
   hotkey_manager_windows
   screen_retriever_windows
-  system_tray
+  tray_manager
   url_launcher_windows
   window_manager
 )
